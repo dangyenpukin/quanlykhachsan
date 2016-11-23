aa1688101054d4cb94114691df3debfcf5bbe166
dangyenpukin <pukin.277@gmail.com>
2016-11-23 23:11:43 +0700

    in hóa đơn vơi thêm khách hàng nhá

---
+++ quanlykhachsan2_cuoi_cung/quanlykhachsan2/ThemKhachHang.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace quanlykhachsan2
+{
+    partial class ThemKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.cbGT = new System.Windows.Forms.ComboBox();
+            this.txtQuocTich = new System.Windows.Forms.TextBox();
+            this.lblQuocTich = new System.Windows.Forms.Label();
+            this.txtNgheNghiep = new System.Windows.Forms.TextBox();
+            this.lblNgheNghiep = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.lblDiaChi = new System.Windows.Forms.Label();
+            this.txtSoCMND = new System.Windows.Forms.TextBox();
+            this.lblSoCMND = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.lblSDT = new System.Windows.Forms.Label();
+            this.lblGT = new System.Windows.Forms.Label();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.lblNgaySinh = new System.Windows.Forms.Label();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.lblTenKH = new System.Windows.Forms.Label();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.Color.Yellow;
+            this.btnTimKiem.Location = new System.Drawing.Point(468, 37);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(96, 40);
+            this.btnTimKiem.TabIndex = 103;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // cbGT
+            // 
+            this.cbGT.FormattingEnabled = true;
+            this.cbGT.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.cbGT.Location = new System.Drawing.Point(195, 172);
+            this.cbGT.Name = "cbGT";
+            this.cbGT.Size = new System.Drawing.Size(164, 21);
+            this.cbGT.TabIndex = 102;
+            // 
+            // txtQuocTich
+            // 
+            this.txtQuocTich.Location = new System.Drawing.Point(195, 340);
+            this.txtQuocTich.Name = "txtQuocTich";
+            this.txtQuocTich.Size = new System.Drawing.Size(164, 20);
+            this.txtQuocTich.TabIndex = 101;
+            // 
+            // lblQuocTich
+            // 
+            this.lblQuocTich.Location = new System.Drawing.Point(26, 343);
+            this.lblQuocTich.Name = "lblQuocTich";
+            this.lblQuocTich.Size = new System.Drawing.Size(75, 13);
+            this.lblQuocTich.TabIndex = 100;
+            this.lblQuocTich.Text = "Quốc Tịch";
+            // 
+            // txtNgheNghiep
+            // 
+            this.txtNgheNghiep.Location = new System.Drawing.Point(195, 296);
+            this.txtNgheNghiep.Name = "txtNgheNghiep";
+            this.txtNgheNghiep.Size = new System.Drawing.Size(164, 20);
+            this.txtNgheNghiep.TabIndex = 99;
+            // 
+            // lblNgheNghiep
+            // 
+            this.lblNgheNghiep.AutoSize = true;
+            this.lblNgheNghiep.Location = new System.Drawing.Point(26, 296);
+            this.lblNgheNghiep.Name = "lblNgheNghiep";
+            this.lblNgheNghiep.Size = new System.Drawing.Size(70, 13);
+            this.lblNgheNghiep.TabIndex = 98;
+            this.lblNgheNghiep.Text = "Nghề Nghiệp";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(195, 260);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(164, 20);
+            this.txtDiaChi.TabIndex = 97;
+            // 
+            // lblDiaChi
+            // 
+            this.lblDiaChi.AutoSize = true;
+            this.lblDiaChi.Location = new System.Drawing.Point(26, 260);
+            this.lblDiaChi.Name = "lblDiaChi";
+            this.lblDiaChi.Size = new System.Drawing.Size(41, 13);
+            this.lblDiaChi.TabIndex = 96;
+            this.lblDiaChi.Text = "Địa Chỉ";
+            // 
+            // txtSoCMND
+            // 
+            this.txtSoCMND.Location = new System.Drawing.Point(165, -35);
+            this.txtSoCMND.Name = "txtSoCMND";
+            this.txtSoCMND.Size = new System.Drawing.Size(164, 20);
+            this.txtSoCMND.TabIndex = 95;
+            // 
+            // lblSoCMND
+            // 
+            this.lblSoCMND.AutoSize = true;
+            this.lblSoCMND.Location = new System.Drawing.Point(2, -28);
+            this.lblSoCMND.Name = "lblSoCMND";
+            this.lblSoCMND.Size = new System.Drawing.Size(55, 13);
+            this.lblSoCMND.TabIndex = 94;
+            this.lblSoCMND.Text = "Số CMND";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(195, 214);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(164, 20);
+            this.txtSDT.TabIndex = 93;
+            // 
+            // lblSDT
+            // 
+            this.lblSDT.AutoSize = true;
+            this.lblSDT.Location = new System.Drawing.Point(26, 217);
+            this.lblSDT.Name = "lblSDT";
+            this.lblSDT.Size = new System.Drawing.Size(75, 13);
+            this.lblSDT.TabIndex = 92;
+            this.lblSDT.Text = "Số Điện Thoại";
+            // 
+            // lblGT
+            // 
+            this.lblGT.AutoSize = true;
+            this.lblGT.Location = new System.Drawing.Point(31, 180);
+            this.lblGT.Name = "lblGT";
+            this.lblGT.Size = new System.Drawing.Size(51, 13);
+            this.lblGT.TabIndex = 91;
+            this.lblGT.Text = "Giới Tính";
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.CustomFormat = "dd/MM/yyyy";
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(195, 132);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(164, 20);
+            this.dtpNgaySinh.TabIndex = 90;
+            // 
+            // lblNgaySinh
+            // 
+            this.lblNgaySinh.AutoSize = true;
+            this.lblNgaySinh.Location = new System.Drawing.Point(31, 138);
+            this.lblNgaySinh.Name = "lblNgaySinh";
+            this.lblNgaySinh.Size = new System.Drawing.Size(56, 13);
+            this.lblNgaySinh.TabIndex = 89;
+            this.lblNgaySinh.Text = "Ngày Sinh";
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.Location = new System.Drawing.Point(195, 84);
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(164, 20);
+            this.txtTenKH.TabIndex = 88;
+            // 
+            // lblTenKH
+            // 
+            this.lblTenKH.Location = new System.Drawing.Point(26, 87);
+            this.lblTenKH.Name = "lblTenKH";
+            this.lblTenKH.Size = new System.Drawing.Size(104, 17);
+            this.lblTenKH.TabIndex = 87;
+            this.lblTenKH.Text = "Tên Khách Hàng";
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Location = new System.Drawing.Point(195, 37);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(164, 20);
+            this.txtMaKH.TabIndex = 86;
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.AutoSize = true;
+            this.lblMaKH.Location = new System.Drawing.Point(31, 37);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(85, 13);
+            this.lblMaKH.TabIndex = 85;
+            this.lblMaKH.Text = "Mã Khách Hàng";
+            // 
+            // ThemKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 426);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.cbGT);
+            this.Controls.Add(this.txtQuocTich);
+            this.Controls.Add(this.lblQuocTich);
+            this.Controls.Add(this.txtNgheNghiep);
+            this.Controls.Add(this.lblNgheNghiep);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.lblDiaChi);
+            this.Controls.Add(this.txtSoCMND);
+            this.Controls.Add(this.lblSoCMND);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.lblSDT);
+            this.Controls.Add(this.lblGT);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.lblNgaySinh);
+            this.Controls.Add(this.txtTenKH);
+            this.Controls.Add(this.lblTenKH);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.lblMaKH);
+            this.Name = "ThemKhachHang";
+            this.Text = "ThemKhachHang";
+            this.Load += new System.EventHandler(this.ThemKhachHang_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.ComboBox cbGT;
+        private System.Windows.Forms.TextBox txtQuocTich;
+        private System.Windows.Forms.Label lblQuocTich;
+        private System.Windows.Forms.TextBox txtNgheNghiep;
+        private System.Windows.Forms.Label lblNgheNghiep;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label lblDiaChi;
+        private System.Windows.Forms.TextBox txtSoCMND;
+        private System.Windows.Forms.Label lblSoCMND;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label lblSDT;
+        private System.Windows.Forms.Label lblGT;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.Label lblNgaySinh;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.Label lblTenKH;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.Label lblMaKH;
+    }
+}
---
+++ quanlykhachsan2_cuoi_cung/quanlykhachsan2/ThemKhachHang.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyKhachSanBUS;
+using QuanLyKhachSanEntity;
+
+namespace quanlykhachsan2
+{
+    public partial class ThemKhachHang : Form
+    {
+        BUS_KhachHang buskh = new BUS_KhachHang();
+        EC_KhachHang eckh = new EC_KhachHang();
+        public ThemKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void ThemKhachHang_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            if (txtSoCMND.Text == "")
+            {
+                MessageBox.Show("Bạn Chưa Nhập Số Chứng Minh Nhân Dân");
+                return;
+            }
+            if (txtSoCMND.Text != "")
+            {
+                int t = 0;
+                DataTable dt = buskh.TaoBang("");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i]["SoCMND"].ToString() == txtSoCMND.Text)
+                    {
+                        MessageBox.Show("Đã Có Sẵn Thông Tin Của Khách Hàng");
+                        t = 1;
+                        txtMaKH.Text = dt.Rows[i]["MaKH"].ToString();
+                        txtTenKH.Text = dt.Rows[i]["TenKH"].ToString();
+                        dtpNgaySinh.Text = dt.Rows[i]["NgaySinh"].ToString();
+                        cbGT.Text = dt.Rows[i]["GT"].ToString();
+                        txtSDT.Text = dt.Rows[i]["SDT"].ToString();
+                        txtDiaChi.Text = dt.Rows[i]["DiaChi"].ToString();
+                        txtNgheNghiep.Text = dt.Rows[i]["NgheNghiep"].ToString();
+                        txtQuocTich.Text = dt.Rows[i]["QuocTich"].ToString();
+                   
+                        break;
+                    }
+                }
+                if (t == 0)
+                {
+                   
+                    MessageBox.Show("Đây Là Khách Hàng Đến Lần Đầu,Bạn Cần Nhập Thông Tin Đầy Đủ Của Khách Hàng Đó");
+                    int makh;
+                    if (buskh.CountID() == 1)
+                    {
+                        makh = (int.Parse(buskh.GetLastID()) + 1);
+                    }
+                    else
+                    {
+                        makh = 0;
+                    }
+                    txtMaKH.Text = makh.ToString();
+                }
+            }
+        }
+    }
+}
