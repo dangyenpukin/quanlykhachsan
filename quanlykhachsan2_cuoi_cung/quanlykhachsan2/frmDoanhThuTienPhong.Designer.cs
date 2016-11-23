88bb354de38c1e4bcdb0361671536e35044360ea
Tuanbita <tuanbita@gmail.com>
2016-11-24 00:04:26 +0700

    frmDoanhThuTienPhong

--- quanlykhachsan2_cuoi_cung/quanlykhachsan2/frmDoanhThuTienPhong.Designer.cs
+++ quanlykhachsan2_cuoi_cung/quanlykhachsan2/frmDoanhThuTienPhong.Designer.cs
@@ -44,26 +44,28 @@
             this.dtcTienPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnTinh = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnThoat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThuTienPhong)).BeginInit();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblNgayDau
             // 
             this.lblNgayDau.AutoSize = true;
-            this.lblNgayDau.Location = new System.Drawing.Point(21, 25);
+            this.lblNgayDau.Location = new System.Drawing.Point(21, 27);
             this.lblNgayDau.Name = "lblNgayDau";
-            this.lblNgayDau.Size = new System.Drawing.Size(58, 13);
+            this.lblNgayDau.Size = new System.Drawing.Size(58, 14);
             this.lblNgayDau.TabIndex = 24;
             this.lblNgayDau.Text = "Ngày Đầu ";
             // 
             // lblNgayCuoi
             // 
             this.lblNgayCuoi.AutoSize = true;
-            this.lblNgayCuoi.Location = new System.Drawing.Point(21, 90);
+            this.lblNgayCuoi.Location = new System.Drawing.Point(21, 97);
             this.lblNgayCuoi.Name = "lblNgayCuoi";
-            this.lblNgayCuoi.Size = new System.Drawing.Size(56, 13);
+            this.lblNgayCuoi.Size = new System.Drawing.Size(56, 14);
             this.lblNgayCuoi.TabIndex = 25;
             this.lblNgayCuoi.Text = "Ngày Cuối";
             // 
@@ -71,7 +73,7 @@
             // 
             this.dtpNgayCuoi.CustomFormat = "dd/MM/yyyy";
             this.dtpNgayCuoi.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpNgayCuoi.Location = new System.Drawing.Point(165, 90);
+            this.dtpNgayCuoi.Location = new System.Drawing.Point(165, 97);
             this.dtpNgayCuoi.Name = "dtpNgayCuoi";
             this.dtpNgayCuoi.Size = new System.Drawing.Size(164, 20);
             this.dtpNgayCuoi.TabIndex = 73;
@@ -80,7 +82,7 @@
             // 
             this.dtpNgayDau.CustomFormat = "dd/MM/yyyy";
             this.dtpNgayDau.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpNgayDau.Location = new System.Drawing.Point(165, 25);
+            this.dtpNgayDau.Location = new System.Drawing.Point(165, 27);
             this.dtpNgayDau.Name = "dtpNgayDau";
             this.dtpNgayDau.Size = new System.Drawing.Size(164, 20);
             this.dtpNgayDau.TabIndex = 74;
@@ -88,32 +90,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(282, 22);
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(542, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(304, 20);
+            this.label1.Size = new System.Drawing.Size(298, 18);
             this.label1.TabIndex = 75;
             this.label1.Text = "Chi Tiết Hóa Đơn Tiền Phòng Đã Thu Tiền";
             // 
             // lblDoanhThuTienPhong
             // 
             this.lblDoanhThuTienPhong.AutoSize = true;
-            this.lblDoanhThuTienPhong.Location = new System.Drawing.Point(21, 158);
+            this.lblDoanhThuTienPhong.Location = new System.Drawing.Point(3, 170);
             this.lblDoanhThuTienPhong.Name = "lblDoanhThuTienPhong";
-            this.lblDoanhThuTienPhong.Size = new System.Drawing.Size(147, 13);
+            this.lblDoanhThuTienPhong.Size = new System.Drawing.Size(142, 14);
             this.lblDoanhThuTienPhong.TabIndex = 76;
             this.lblDoanhThuTienPhong.Text = "Tổng Doanh Thu Tiền Phòng";
             // 
             // txtTongDoanhThuTienPhong
             // 
-            this.txtTongDoanhThuTienPhong.Location = new System.Drawing.Point(165, 155);
+            this.txtTongDoanhThuTienPhong.Location = new System.Drawing.Point(165, 167);
             this.txtTongDoanhThuTienPhong.Name = "txtTongDoanhThuTienPhong";
             this.txtTongDoanhThuTienPhong.Size = new System.Drawing.Size(164, 20);
             this.txtTongDoanhThuTienPhong.TabIndex = 77;
             // 
             // dgvDoanhThuTienPhong
             // 
-            this.dgvDoanhThuTienPhong.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.dgvDoanhThuTienPhong.BackgroundColor = System.Drawing.Color.Silver;
             this.dgvDoanhThuTienPhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDoanhThuTienPhong.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dtcMaCTHD,
@@ -122,9 +124,9 @@
             this.dtcNgayDen,
             this.dtcNgayDi,
             this.dtcTienPhong});
-            this.dgvDoanhThuTienPhong.Location = new System.Drawing.Point(356, 76);
+            this.dgvDoanhThuTienPhong.Location = new System.Drawing.Point(345, 58);
             this.dgvDoanhThuTienPhong.Name = "dgvDoanhThuTienPhong";
-            this.dgvDoanhThuTienPhong.Size = new System.Drawing.Size(519, 222);
+            this.dgvDoanhThuTienPhong.Size = new System.Drawing.Size(651, 420);
             this.dgvDoanhThuTienPhong.TabIndex = 78;
             // 
             // dtcMaCTHD
@@ -147,7 +149,7 @@
             this.dtcSoPhong.DataPropertyName = "SoPhong";
             this.dtcSoPhong.HeaderText = "Số Phòng";
             this.dtcSoPhong.Name = "dtcSoPhong";
-            this.dtcSoPhong.Width = 73;
+            this.dtcSoPhong.Width = 72;
             // 
             // dtcNgayDen
             // 
@@ -179,28 +181,38 @@
             this.panel1.Controls.Add(this.txtTongDoanhThuTienPhong);
             this.panel1.Controls.Add(this.lblNgayCuoi);
             this.panel1.Controls.Add(this.dtpNgayCuoi);
-            this.panel1.Location = new System.Drawing.Point(0, 77);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(350, 221);
+            this.panel1.Size = new System.Drawing.Size(342, 530);
             this.panel1.TabIndex = 79;
             // 
             // btnTinh
             // 
             this.btnTinh.BackColor = System.Drawing.Color.Yellow;
-            this.btnTinh.Location = new System.Drawing.Point(112, 258);
+            this.btnTinh.Location = new System.Drawing.Point(112, 278);
             this.btnTinh.Name = "btnTinh";
-            this.btnTinh.Size = new System.Drawing.Size(75, 35);
+            this.btnTinh.Size = new System.Drawing.Size(75, 38);
             this.btnTinh.TabIndex = 79;
             this.btnTinh.Text = "Tính";
             this.btnTinh.UseVisualStyleBackColor = false;
             this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnThoat);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(342, 476);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(654, 54);
+            this.panel2.TabIndex = 80;
+            // 
             // btnThoat
             // 
             this.btnThoat.BackColor = System.Drawing.Color.Red;
-            this.btnThoat.Location = new System.Drawing.Point(384, 327);
+            this.btnThoat.Location = new System.Drawing.Point(286, 0);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 50);
+            this.btnThoat.Size = new System.Drawing.Size(75, 54);
             this.btnThoat.TabIndex = 0;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = false;
@@ -208,11 +220,11 @@
             // 
             // frmDoanhThuTienPhong
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Cyan;
-            this.ClientSize = new System.Drawing.Size(878, 378);
-            this.Controls.Add(this.btnThoat);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(996, 530);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgvDoanhThuTienPhong);
             this.Controls.Add(this.label1);
@@ -222,6 +234,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThuTienPhong)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -244,6 +257,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dtcNgayDi;
         private System.Windows.Forms.DataGridViewTextBoxColumn dtcTienPhong;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.Button btnTinh;
     }
